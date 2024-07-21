3feab51dbbbe77decb7a385ad77ced1bcad40436
synth <wavydave2000@gmail.com>
2024-07-21 18:04:15 -0400

    remove warp rando

--- ChaoticMix/ChaoticMod.cs
+++ ChaoticMix/ChaoticMod.cs
@@ -8,23 +8,7 @@ namespace ChaoticMix
     {
         public ChaoticMod(ModInterface mi) : base(mi) { }
 
-        public override void warpTableUpdate(WarpTable wt)
-        {
-            Warp[] warps = wt.Warps;
-            Random r = new Random();
-            r.Shuffle(warps);
-            for(int i = 0; i < warps.Length; i++)
-            {
-                Warp currentEdit = wt.Warps[i];
-                currentEdit.RoomID = warps[i].RoomID;
-                currentEdit.PlayerPosition = warps[i].PlayerPosition;
-                currentEdit.Party1Position = warps[i].Party1Position;
-                currentEdit.Party2Position = warps[i].Party2Position;
-                currentEdit.PlayerRotation = warps[i].PlayerRotation;
-                currentEdit.Party1Rotation = warps[i].Party1Rotation;
-                currentEdit.Party2Rotation = warps[i].Party2Rotation;
-            }
-        }
+        
 
         public override void playerLoaded(Entity newPlayer)
         {
